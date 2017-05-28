91c573b2ba092f40df7762537ddd96f0d7f803ee
Martin <flextry@gmail.com>
2017-05-28 13:54:11 +0300

    Removed unnecessary classes and project references

---
+++ Scheduler.TaskPrioritization/Infrastructure/Filters/ExecutionTimeFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace Scheduler.TaskPrioritization.Infrastructure.Filters
+{
+    using System.Diagnostics;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Filters;
+
+    public class ExecutionTimeFilterAttribute : ActionFilterAttribute
+    {
+        private const string StopwatchKey = "StopwatchFilter.Value";
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            actionContext.Request.Properties[StopwatchKey] = Stopwatch.StartNew();
+
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnActionExecuted(actionExecutedContext);
+
+            var stopwatch = (Stopwatch)actionExecutedContext.Request.Properties[StopwatchKey];
+            stopwatch.Stop();
+
+            actionExecutedContext.Response.Headers.Add("Execution-Time", stopwatch.ElapsedMilliseconds.ToString());
+        }
+    }
+}
---
+++ Scheduler.TaskPrioritization/Infrastructure/Tasks/CalculatorFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Scheduler.TaskPrioritization.Infrastructure.Tasks
+{
+    using System.Diagnostics;
+
+    public static class CalculatorFactory
+    {
+        public static CatalanNumbersCalculator StartNew(int startValue = 0, int endValue = 500)
+        {
+            var sw = Stopwatch.StartNew();
+
+            var catalanNumbersCalculator = new CatalanNumbersCalculator()
+            {
+                StartValue = startValue,
+                EndValue = endValue
+            };
+
+            catalanNumbersCalculator.Execute();
+
+            sw.Stop();
+            catalanNumbersCalculator.ElapsedTime = sw.Elapsed;
+
+            return catalanNumbersCalculator;
+        }
+    }
+}
